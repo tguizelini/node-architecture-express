d55d130e382da346cfa7320d63d9f1c2412d6591
Tiago Guizelini <tiago.guizelini@7comm.com.br>
2019-12-06 17:40:21 -0300

    refact folders

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-const authRoutes = require('./auth.route')
+const authRoutes = require('./auth.routes')
 
 module.exports = app => {
   router.get('/', (req, res) => res.json('NodeRest API running'))

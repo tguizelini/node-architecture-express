ffed0c69ea39c9275b9e622cb8fb4498e69acf3b
Tiago Guizelini <tiago.guizelini@7comm.com.br>
2019-12-06 17:39:18 -0300

    refact folders

--- server.js
+++
@@ -1,2 +0,0 @@
-const app = require('./src/config/app')()
-app.listen(app.get('port'), () => console.log(`Server running on port ${app.get('port')}`))
---
+++ src/server.js
@@ -0,0 +1,2 @@
+const app = require('./config/app')()
+app.listen(app.get('port'), () => console.log(`Server running on port ${app.get('port')}`))

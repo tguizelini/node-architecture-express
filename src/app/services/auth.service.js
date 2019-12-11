dce384e41e6828acb091b0e2df551b7daf634bc8
Tiago Guizelini <tiago.guizelini@7comm.com.br>
2019-12-10 21:08:06 -0300

    refact names

--- src/app/services/auth.service.js
+++ src/app/services/auth.service.js
@@ -1,11 +1,11 @@
-const UseRepository = require('../repositories/auth.repository')
+const AuthRepository = require('../repositories/auth.repository')
 const HttpResponse = require('../helpers/http-response')
 const bcrypt = require('bcryptjs')
 const JwtHelper = require('../helpers/jwt-helper')
 
 class AuthService {
   async auth (email, password) {
-    const user = await UseRepository.findOne({ email }).select('+password')
+    const user = await AuthRepository.findOne({ email }).select('+password')
 
     if (!user) return HttpResponse.forbiden(user, 'User not found')
 
@@ -20,11 +20,11 @@ class AuthService {
   }
 
   async register (name, email, password) {
-    const checkExists = await UseRepository.findOne({ email })
+    const checkExists = await AuthRepository.findOne({ email })
 
     if (checkExists) return HttpResponse.forbiden(null, 'User already exists')
 
-    const user = await UseRepository.create({ name, email, password })
+    const user = await AuthRepository.create({ name, email, password })
     user.password = undefined
 
     const token = JwtHelper.generateToken(user.id)
